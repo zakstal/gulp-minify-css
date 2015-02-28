0e15666574dbb4bd0915c71ba2812e93b181de1a
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-03-01 00:46:57 +0900

    introduce vinyl-bufferstream
    https://github.com/shinnn/vinyl-bufferstream

--- index.js
+++ index.js
@@ -1,136 +1,100 @@
 'use strict';
 
-var path = require('path'),
-  applySourceMap = require('vinyl-sourcemaps-apply'),
-  BufferStreams = require('bufferstreams'),
-  cache = require('memory-cache'),
-  CleanCSS = require('clean-css'),
-  PluginError = require('gulp-util').PluginError,
-  through2 = require('through2');
+var path = require('path');
+
+var applySourceMap = require('vinyl-sourcemaps-apply');
+var cache = require('memory-cache');
+var CleanCSS = require('clean-css');
+var PluginError = require('gulp-util').PluginError;
+var through2 = require('through2');
+var VinylBufferStream = require('vinyl-bufferstream');
 
 function objectIsEqual(a, b) {
   return JSON.stringify(a) === JSON.stringify(b);
 }
 
-function minify(options, file, buffer, done) {
-  var rawContents = String(buffer);
-  var cached;
-  if (options.cache &&
-    (cached = cache.get(file.path)) &&
-    cached.raw === rawContents &&
-    objectIsEqual(cached.options, options)) {
+function sourceMap(file, map) {
+  map.file = path.relative(file.base, file.path);
+  map.sources = map.sources.map(function(src) {
+    if (src === '__stdin__.css') {
+      return path.relative(file.base, file.path);
+    } else if (path.resolve(src) === path.normalize(src)) {
+      // Path is absolute so imported file had no existing source map.
+      // Trun absolute path in to path relative to file.base.
+      return path.relative(file.base, src);
+    } else {
+      return src;
+    }
+  });
 
-    // cache hit
-    done(null, cached.minified);
+  applySourceMap(file, map);
+}
 
-  } else {
-    // cache miss or cache not enabled
-    new CleanCSS(options).minify(rawContents, function(errors, css) {
-      if (options.cache) {
-        cache.put(file.path, {
-          raw: rawContents,
-          minified: css,
-          options: options
-        });
-      }
+module.exports = function gulpMinifyCSS(options) {
+  options = options || {};
 
-      if (errors) {
-        done(new PluginError('minify-css', errors.join(', ')));
+  return through2.obj(function modifyContents(file, enc, cb) {
+    var run = new VinylBufferStream(function(buf, done) {
+      var rawContents = String(buf);
+      var cached;
+      if (options.cache &&
+        (cached = cache.get(file.path)) &&
+        cached.raw === rawContents &&
+        objectIsEqual(cached.options, options)) {
+
+        // cache hit
+        done(null, new Buffer(cached.minified.styles));
         return;
       }
 
-      done(null, css);
-    });
-  }
-}
+      // Image URLs are rebased with the assumption that they are relative to the
+      // CSS file they appear in (unless "relativeTo" option is explicitly set by
+      // caller)
+      var relativeToTmp = options.relativeTo;
+      options.relativeTo = options.relativeTo || path.resolve(path.dirname(file.path));
 
-// File level transform function
-function minifyCSSTransform(opt, file) {
-  return function(err, buf, cb) {
-    if (err) {
-      cb(new PluginError('minify-css', err));
-      return;
-    }
-
-    // Use the buffered content
-    minify(opt, file, buf, function(errors, data) {
-      if (errors) {
-        cb(new PluginError('minify-css', errors));
-        return;
+      // Enable sourcemap support if initialized file comes in.
+      if (file.sourceMap) {
+        options.sourceMap = JSON.stringify(file.sourceMap);
       }
-      cb(null, new Buffer(data.styles));
+
+      new CleanCSS(options).minify(rawContents, function(errors, css) {
+        if (options.cache) {
+          cache.put(file.path, {
+            raw: rawContents,
+            minified: css,
+            options: options
+          });
+        }
+
+        if (errors) {
+          done(new PluginError('minify-css', errors.join(', '), {fileName: file.path}));
+          return;
+        }
+
+        // Restore original "relativeTo" value
+        options.relativeTo = relativeToTmp;
+
+        if (css.sourceMap && file.sourceMap) {
+          // clean-css gives bad 'sources' and 'file' properties because we
+          // pass in raw css instead of a file.  So we fix those here.
+          sourceMap(file, JSON.parse(css.sourceMap));
+        }
+
+        done(null, new Buffer(css.styles));
+      });
     });
-  };
-}
 
-// Plugin function
-function minifyCSSGulp (opt) {
-  opt = opt || {};
+    var self = this;
 
-  function modifyContents(file, enc, done) {
-    if (file.isNull()) {
-      done(null, file);
-      return;
-    }
-
-    if (file.isStream()) {
-      file.contents = file.contents.pipe(new BufferStreams(minifyCSSTransform(opt, file)))
-      .on('error', this.emit.bind(this, 'error'));
-      done(null, file);
-      return;
-    }
-
-    // Image URLs are rebased with the assumption that they are relative to the
-    // CSS file they appear in (unless "relativeTo" option is explicitly set by
-    // caller)
-    var relativeToTmp = opt.relativeTo;
-    opt.relativeTo = opt.relativeTo || path.resolve(path.dirname(file.path));
-
-    // Enable sourcemap support if initialized file comes in.
-    if (file.sourceMap) {
-      opt.sourceMap = JSON.stringify(file.sourceMap);
-    }
-
-    minify(opt, file, file.contents, function(err, newContents) {
+    run(file, function(err, contents) {
       if (err) {
-        done(err);
-        return;
+        self.emit('error', err);
+      } else {
+        file.contents = contents;
+        self.push(file);
       }
-
-      // Restore original "relativeTo" value
-      opt.relativeTo = relativeToTmp;
-      //console.log(newContents);
-      file.contents = new Buffer(newContents.styles);
-
-      if (newContents.sourceMap && file.sourceMap) {
-        // clean-css gives bad 'sources' and 'file' properties because we
-        // pass in raw css instead of a file.  So we fix those here.
-        var map = JSON.parse(newContents.sourceMap);
-        map.file = path.relative(file.base, file.path);
-        map.sources = map.sources.map(function(src) {
-          if (src === '__stdin__.css') {
-            return path.relative(file.base, file.path);
-          } else if (path.resolve(src) === path.normalize(src)) {
-            // Path is absolute so imported file had no existing source map.
-            // Trun absolute path in to path relative to file.base.
-            return path.relative(file.base, src);
-          } else {
-            return src;
-          }
-        });
-
-        applySourceMap(file, map);
-      }
-
-      done(null, file);
+      cb();
     });
-  }
-
-  return through2.obj(modifyContents);
-}
-
-// Export the file level transform function for other plugins usage
-minifyCSSGulp.fileTransform = minifyCSSTransform;
-
-// Export the plugin main function
-module.exports = minifyCSSGulp;
+  });
+};
