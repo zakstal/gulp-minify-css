194f671b153e07c3684932604d88b134b69ea7f8
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-03-10 00:41:00 +0900

    remove cache option
    close #75

--- index.js
+++ index.js
@@ -3,15 +3,11 @@
 var path = require('path');
 
 var applySourceMap = require('vinyl-sourcemaps-apply');
-var cache = require('memory-cache');
 var CleanCSS = require('clean-css');
+var objectAssign = require('object-assign');
 var PluginError = require('gulp-util').PluginError;
 var through2 = require('through2');
 var VinylBufferStream = require('vinyl-bufferstream');
-
-function objectIsEqual(a, b) {
-  return JSON.stringify(a) === JSON.stringify(b);
-}
 
 function sourceMap(file, map) {
   map.file = path.relative(file.base, file.path);
@@ -35,45 +31,23 @@ module.exports = function gulpMinifyCSS(options) {
 
   return through2.obj(function modifyContents(file, enc, cb) {
     var run = new VinylBufferStream(function(buf, done) {
-      var rawContents = String(buf);
-      var cached;
-      if (options.cache &&
-        (cached = cache.get(file.path)) &&
-        cached.raw === rawContents &&
-        objectIsEqual(cached.options, options)) {
-
-        // cache hit
-        done(null, new Buffer(cached.minified.styles));
-        return;
-      }
+      var fileOptions = objectAssign({}, options);
 
       // Image URLs are rebased with the assumption that they are relative to the
       // CSS file they appear in (unless "relativeTo" option is explicitly set by
       // caller)
-      var relativeToTmp = options.relativeTo;
-      options.relativeTo = options.relativeTo || path.resolve(path.dirname(file.path));
+      fileOptions.relativeTo = options.relativeTo || path.resolve(path.dirname(file.path));
 
       // Enable sourcemap support if initialized file comes in.
       if (file.sourceMap) {
-        options.sourceMap = JSON.stringify(file.sourceMap);
+        fileOptions.sourceMap = JSON.stringify(file.sourceMap);
       }
 
-      new CleanCSS(options).minify(rawContents, function(errors, css) {
-        if (options.cache) {
-          cache.put(file.path, {
-            raw: rawContents,
-            minified: css,
-            options: options
-          });
-        }
-
+      new CleanCSS(fileOptions).minify(String(buf), function(errors, css) {
         if (errors) {
-          done(new PluginError('minify-css', errors.join(', '), {fileName: file.path}));
+          done(errors.join(' '));
           return;
         }
-
-        // Restore original "relativeTo" value
-        options.relativeTo = relativeToTmp;
 
         if (css.sourceMap && file.sourceMap) {
           // clean-css gives bad 'sources' and 'file' properties because we
@@ -89,7 +63,7 @@ module.exports = function gulpMinifyCSS(options) {
 
     run(file, function(err, contents) {
       if (err) {
-        self.emit('error', err);
+        self.emit('error', new PluginError('gulp-minify-css', err, {fileName: file.path}));
       } else {
         file.contents = contents;
         self.push(file);
